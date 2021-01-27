604858b5a0cb43f5c80962f07f122350b1a26dee
unknown <you@example.com>
2021-01-26 22:00:57 -0700

    initial commit

---
+++ MagicEightBall.js
@@ -0,0 +1,34 @@
+let userName ='';
+
+'Jane' ? console.log('Hello Jane!') : Console.log('Hello!');
+
+let userQuestion = 'What is the magic number?';
+
+console.log(userName + userQuestion);
+
+const randomNumber = Math.floor(Math.random() * 8);
+console.log(randomNumber);
+let eightBall = '';
+
+if (randomNumber === 0) {
+  let eightBall = console.log('Nah. Not at all');
+} else if (randomNumber === 1) {
+  let eightBall = console.log('At Little Closer');
+} else if (randomNumber === 2) {
+  let eightBall = console.log('Getting Warmer');
+}  else if (randomNumber === 3) {
+  let eightBall = console.log('Bit Wincy Warmer');
+} else if (randomNumber === 4) {
+  let eightBall = console.log('Half Warm Half Cold');
+}  else if (randomNumber === 5) {
+  let eightBall = console.log('Now you talking');
+} else if (randomNumber === 6) {
+  let eightBall = console.log('Almost There');
+}  else if (randomNumber === 7) {
+  let eightBall = console.log('One More Step to Success');
+} else if (randomNumber === 6) {
+  let eightBall = console.log('The Golden Magic Eight Ball Wohooo!');
+}
+
+
+
